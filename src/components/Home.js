2606c76090d3073797bf4bf9ece637977f0473b8
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-11-28 16:14:49 -0500

    set up tailwindcss

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,10 @@ function Home() {
 
 return (
     <div>
-        <h1>HOME</h1>
+        <h1 className="">HOME</h1>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
 )
 }
