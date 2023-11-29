70d0565d032f1d3edba29c1f799a779f06bd4af7
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-11-29 16:05:46 -0500

    working on footer and links

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" element={<Home />}/>
           <Route path="/story" element={<MyStory />}/>
         </Routes>
-      <Footer />
+        <Footer id="footer"/>
       </BrowserRouter>
       
     </div>
